06004642ca76d02ac688fe1063592ee072a9490b
Sergei_L <lubeshkosergei@gmail.com>
2021-05-23 22:46:51 +0300

    create in views.py function DetailRooms.

--- hotel/views.py
+++ hotel/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from hotel.models import Room
+from hotel.models import Room, Facilities
 
 
 def ShowRooms(request):
@@ -8,7 +8,10 @@ def ShowRooms(request):
     context = {"rooms": rooms}
     return render(request, "hotel/index.html", context=context)
 
+
 def DetailRooms(request, room_id):
     detail = Room.objects.get(id=room_id)
-    context = {"rooms": detail}
+    facilit = detail.facilities.all()
+    print(len(facilit))
+    context = {"detail": detail, "facilitie": facilit}
     return render(request, "hotel/detailroom.html", context=context)
