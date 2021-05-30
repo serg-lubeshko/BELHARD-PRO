bbbdbdc4291eed74aed429867e5b79abfc62ea90
Sergei_L <lubeshkosergei@gmail.com>
2021-05-30 14:51:47 +0300

    add check date_arival > date_departure

--- hotel/views.py
+++ hotel/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 from hotel.forms import BookingForm, ServiceHotelForm
 from hotel.models import Room, Facilities, BokkingRoom, TypeService, ServiceHotel
 
@@ -20,46 +19,34 @@ def detailRooms(request, room_id):
     return render(request, template_name="hotel/detailroom.html", context=context)
 
 
-def check_date(room_booking, date_arrival,date_departure):
+def check_date(room_booking, date_arrival, date_departure):
     for dates in room_booking:
         date_1 = str(dates[0])
-        # print(date_1, 'date1')
         date_2 = str(dates[1])
-        # print(type(date_arrival),'hhhhhhhhhhhhhhhhhhh')
+        if date_arrival > date_departure:
+            return "Date"
         if date_arrival > date_2:
             return True
         elif date_departure < date_1:
             return True
-        elif date_1<=date_arrival <= date_2:
+        elif date_1 <= date_arrival <= date_2:
             return False
         else:
             return False
-        # print(date_1, 'date_1')
-        # print(date_2, 'date_2')
 
 
 def booking(request, room_id):
     room = Room.objects.get(id=room_id)
-    # print(room_booking)
-    # print(request.POST["date_arrival"], 'mmmmmm')
-    # print(request.POST["date_departure"], 'mmmmmmmmmmmmmmm')
-    # xx = BokkingRoom.objects.values_list("date_arrival", 'date_departure')
-    # print(xx)
-    # a = xx[0][0]
-    # b = xx[1][1]
-    # print(xx[0][0], "effeeefw")
-    # print(xx[1][1], "aaaaaaaa")
-    # if a > b:
-    #     print('true')
     if request.method == "POST":
         room_booking = room.booking_room.values_list("date_arrival", 'date_departure')
         date_arriv = request.POST["date_arrival"]
-        # print(date_arriv, "ewfwfefewfwefewfwefwefwefwe")
         date_depar = request.POST["date_departure"]
-        # print(date_depar, 'wdwqdwqdwqdwqdwqdwqdwqdwdwqdwq')
         result = check_date(room_booking, date_arriv, date_depar)
-        if result==False:
+        if result == False:
             messages.error(request, "Номер в указанные даты занят")
+            form = BookingForm()
+        elif result == "Date":
+            messages.error(request, "Не правильно указаны даты")
             form = BookingForm()
         else:
             form = BookingForm(request.POST)
@@ -75,15 +62,14 @@ def booking(request, room_id):
     context = {"form": form, "room": room}
     return render(request, template_name="hotel/booking.html", context=context)
 
+
 def service(request):
     serv = TypeService.objects.all()
     len_serv = len(serv)
-    form = ServiceHotelForm()
-    form = ServiceHotelForm()
     if request.method == "POST":
         user = request.user.id
         ServiceHotel.objects.filter(users_id=user).delete()
-        for item in range(1,len_serv+1):
+        for item in range(1, len_serv + 1):
             dictmodel = {}
             ServiceHotel.objects.filter(users_id=user).update(**dictmodel)
             mark = request.POST[str(item)]
@@ -91,21 +77,5 @@ def service(request):
             dictmodel["type_id"] = item
             dictmodel["users_id"] = user
             ServiceHotel.objects.create(**dictmodel)
-        messages.success(request,"Спасибо за Вашу оценку!")
-        # if ServiceHotel.objects.filter(users_id=user):
-        #     print("efwefwee")
-        #     A= ServiceHotel.objects.filter(type_id=item, users_id=user).update(**dictmodel)
-        #     print(A,"fwefwe")
-        # else:
-        #     print("aaa")
-        #     ServiceHotel.objects.create(**dictmodel)
-
-
-
-
-            # ServiceHotel.objects.update_or_create(type_id=user, defaults=dictmodel)
-
-
-
-
-    return render(request, 'hotel/service.html', context={ 'services':serv})
+        messages.success(request, "Спасибо за Вашу оценку!")
+    return render(request, 'hotel/service.html', context={'services': serv})
