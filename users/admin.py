7ea06bc6b1f4314c0a64bd6d7abdd7363febcd80
Sergei_L <lubeshkosergei@gmail.com>
2021-06-04 00:37:15 +0300

    create users and booking table

---
+++ users/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['first_name','email', 'username',]
+    list_display_links = ['first_name','email', 'username',]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+# Register your models here.
